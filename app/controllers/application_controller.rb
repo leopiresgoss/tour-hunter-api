38191bbe366da82035b00b437f34435d792c1fcb
yersel500 <yersel500@gmail.com>
2022-07-08 11:38:47 -0500

    Add registrations controller for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
-class ApplicationController < ActionController::API
+class ApplicationController < ActionController::Base
+  before_action :update_allowed_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:full_name, :role, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:full_name, :role, :email, :password, :current_password)
+    end
+  end
 end
