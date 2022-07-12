a5879f0835e77eb7ce2e37617cca8a51f4802968
behnam1369 <behnam.aghaali@yahoo.com>
2022-07-13 01:11:22 +0430

    Add role to the response of sign in action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :update_allowed_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  respond_to :json
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
