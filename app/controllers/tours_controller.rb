bd9edf4f62e322361126aee4a9f0ee2d50f7b10d
yersel500 <yersel500@gmail.com>
2022-07-12 17:22:31 -0500

    Add authentication for tours controller

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,5 +1,6 @@
 class ToursController < ApplicationController
   before_action :set_tour, only: %i[show update destroy]
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   # GET /tours
   def index
@@ -15,29 +16,40 @@ class ToursController < ApplicationController
 
   # POST /tours
   def create
-    @tour = Tour.new(tour_params)
-    @tour.user = current_user
-    if @tour.save
-      render json: @tour, status: :created, location: @tour
+    if current_user.role == 'Admin'
+      @tour = Tour.new(tour_params)
+      @tour.user = current_user
+      if @tour.save
+        render json: { message: 'Tour was created succesfully.' }, status: :created, location: @tour
+      else
+        render json: @tour.errors, status: :unprocessable_entity
+      end
     else
-      puts 'Testing'
-      render json: @tour.errors, status: :unprocessable_entity
+      render json: { message: "You don\'t have permission to create tours." }, status: :unauthorized
     end
   end
 
   # PATCH/PUT /tours/1
   def update
-    if @tour.update(tour_params)
-      render json: @tour
+    if current_user.role == 'Admin'
+      if @tour.update(tour_params)
+        render json: { message: 'Tour was updated succesfully.' }
+      else
+        render json: @tour.errors, status: :unprocessable_entity
+      end
     else
-      render json: @tour.errors, status: :unprocessable_entity
+      render json: { message: "You don\'t have permission to update tours." }, status: :unauthorized
     end
   end
 
   # DELETE /tours/1
   def destroy
-    @tour.destroy
-    render json: { message: 'Deleted successfully.' }, status: :ok
+    if current_user.role == 'Admin'
+      @tour.destroy
+      render json: { message: 'Deleted successfully.' }, status: :ok
+    else
+      render json: { message: "You don\'t have permission to delete tours." }, status: :unauthorized
+    end
   end
 
   private
