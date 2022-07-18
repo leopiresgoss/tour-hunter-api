8f0244fe62b0acc9707e316d15bb029360d76a0a
mengiefen <mengiefen@gmail.com>
2022-07-18 19:54:54 +0300

    fix bugs to fetch image

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,4 @@
-require_relative '../helpers/reservations_helper'
-
 class ReservationsController < ApplicationController
-  include ReservationHelper
   before_action :set_reservation, only: %i[show update destroy]
 
   # GET /reservations
@@ -34,12 +31,22 @@ class ReservationsController < ApplicationController
   end
 
   # my_reservations
-  def my_reservations 
-
+  def my_reservations
+    sql = "SELECT r.id as reservation_id, r.package, r.tour_date_id,
+    td.date as tour_date, t.id as tour_id, t.name as tour_name,
+    t.description as tour_description,
+    t.price as tour_price, t.location as tour_location,
+    u.id as user_id, u.full_name, u.email as user_email
+    FROM reservations  as r INNER JOIN
+    tour_dates as td ON r.tour_date_id=td.id INNER JOIN
+    tours as t ON td.tour_id=t.id  INNER JOIN
+    users as u ON r.user_id=u.id
+    WHERE r.user_id = #{current_user.id}"
+    reserves = ActiveRecord::Base.connection.execute(sql)
     reserves = fetch_my_reservations
-    @my_reserves = [] 
+    @my_reserves = []
     reserves.each do |reservation|
-      @images = Tour.find_by_id(reservation['tour_id']).images_attachments
+      @images = Tour.find_by_id(reservation['tour_id']).image_urls
       @my_reserves <<
       {
         reservation_id: reservation['reservation_id'],
@@ -56,8 +63,8 @@ class ReservationsController < ApplicationController
         full_name: reservation['full_name'],
         user_email: reservation['user_email']
       }
-    end 
-    render json: @my_reserves, status: :ok   
+    end
+    render json: @my_reserves, status: :ok
   end
 
   private
